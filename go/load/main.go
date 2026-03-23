7020133adb71b7d78b424155e48ef45ff3b89f42
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-03-23 15:04:22 +0000

    Two new subcommands.

--- go/load/main.go
+++ go/load/main.go
@@ -14,10 +14,13 @@ var trimmedArgs []string = os.Args[1:]
 
 func main() {
 	utils.PrintBanner()
-	if len(trimmedArgs) == 0 {
+	if len(trimmedArgs) == 0 || trimmedArgs[0] == "help" {
 		help.ShowUsage("./load <template> <targets>")
 		help.ShowHelp("load")
-		if utils.IsDir("data") {
+		if len(trimmedArgs) != 0 {
+			print("Skipped listing.\n")
+			os.Exit(0)
+		} else if utils.IsDir("data") {
 			var dataFiles, _ = os.ReadDir(utils.GetPath("data"))
 			for _, dataFile := range dataFiles {
 				var fileName = dataFile.Name()
@@ -30,7 +33,13 @@ func main() {
 		} else {
 			utils.PrintLevel(utils.LogError, "The data directory does not exist. Please create and populate the directory first.")
 		}
+		if !utils.IsDir("conf") {
+			utils.PrintLevel(utils.LogError, "The config directory does not exist. Please create and populate the directory first.")
+		}
 		os.Exit(1)
+	} else if trimmedArgs[0] == "version" {
+		help.ShowHelp("copyright")
+		os.Exit(0)
 	} else if utils.IsFile("data/" + trimmedArgs[0] + ".json") {
 		help.ShowUsage("./load " + trimmedArgs[0] + " <targets>")
 		help.ShowHelp("loadTarget")
@@ -47,7 +56,9 @@ func main() {
 		for _, target := range targetsForSorting {
 			print("- " + target + "\n")
 		}
+		os.Exit(1)
 	} else {
 		utils.PrintLevel(utils.LogError, "The specified template \"" + trimmedArgs[0] + "\" does not exist.")
+		os.Exit(1)
 	}
 }
--- sh/gots.sh
+++ sh/gots.sh
@@ -3,6 +3,7 @@ args=( "$@" )
 # Go - test single
 shx gobs "${1}"
 if [ "$?" == "0" ]; then
-	"./build/${1}" "${args[@]:1}"
+	cd example
+	"../build/${1}" "${args[@]:1}"
 fi
 exit
