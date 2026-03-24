735a3e7200d37edfae48eac063e50eae543725cd
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2026-03-24 12:40:53 +0000

    Allows quick builds for per-push runs.

--- sh/build.sh
+++ sh/build.sh
@@ -3,7 +3,7 @@
 export CGO_ENABLED=0
 rm -r "./build/releases" 2>/dev/null
 mkdir -p "./build/releases"
-cat conf/goTargets.txt | while IFS= read -r GOUNION; do
+cat conf/go-${1:-release}.txt | while IFS= read -r GOUNION; do
 	export GOOS=$(printf $GOUNION | cut -d'/' -f1)
 	export GOARCH=$(printf $GOUNION | cut -d'/' -f2)
 	buildDir="./build/$GOOS-$GOARCH"
